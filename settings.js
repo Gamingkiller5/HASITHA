97a48fb05bc8fda66cde3f94c9f17efa4ecf5182
Gamingkiller5 <Gamingkiller1411@gmail.com>
2025-04-13 08:28:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fqQVTD5b#skTzm77gTMRMYd8g8vcXqQmtMyIfyudHQ8b5EDL0j7Y' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=X34hTLAY#DQtHIVUkv9YJREB6H-MzCN9PRi815mPqaU4sQm7tigc' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
